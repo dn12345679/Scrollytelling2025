3436d0d7f232d517c19ef7b2de1ce3f7e3841da3
dn12345679 <dylandnnguyen@gmail.com>
2025-10-07 00:17:43 -0700

    CHECKPOINT: Complete overhaul from climatechange to deforestation

--- main.js
+++ main.js
@@ -14,11 +14,7 @@ window.onload = function () {
     };
 
 // create objects once the DOM loads everything for safety
-window.addEventListener('DOMContentLoaded', () => {
-
-    
-
-});
+window.addEventListener('DOMContentLoaded', () => {});
 
 
 
@@ -26,33 +22,40 @@ var scrolly = document.querySelector('.main-section');
 var step = scrolly.querySelectorAll(".step");
 
 function handleStepProgress(response) {
-    const iceberg = document.querySelector('#part-1-env');
-    const stepHeight = response.element.offsetHeight;
-    const moveX = stepHeight * 0.2 * response.progress; 
-    //iceberg.style.transform = `translateX(${moveX}px)`;
-
-    const melting = iceberg.querySelector('#melting-iceberg');
-    let rotX = Math.sin(32 * Math.PI * response.progress)
-    console.log(rotX * 50)
-    melting.style.transform = `rotate(${rotX * 4}deg)`;
-    console.log(melting.style.transform)
-
-    if (response.progress > 0.5) {
-        const penguins = iceberg.querySelectorAll('.penguin');
-        for (let i = 0; i < penguins.length; i++) {
-            penguins[i].style.transform = 'scaleX(1)'
-            penguins[i].src = './Assets/Penguin_shock.png'
-        }
+    
+    switch (response.element.id) {
+        case 'part-1':
+            scene1(response)
     }
-
 }
+
+
+function scene1(response) {
+    var env = scrolly.querySelector('#part-1-env');
+    var tree = scrolly.querySelector('.tree');
+    var ground = scrolly.querySelector('.ground');
+    if (response.progress > 0.1) {
+        env.classList.add('shift-right')
+        tree.classList.add('shift-right')
+        ground.classList.add('shift-right')
+    }
+    else {
+        env.classList.remove('shift-right')
+        tree.classList.remove   ('shift-right')
+        ground.classList.remove('shift-right')
+    }
+}
+
+
+
+
 
 function resize() {
     var min = window.innerHeight * 0.5;
     var h = min + Math.random() * window.innerHeight * 0.25;
     step.style("height", Math.floor(h) + "px");
     scroller.resize();
-}
+}   
 
 
 function init() {
@@ -67,4 +70,3 @@ function init() {
 
 // kick things off
 init();
-
