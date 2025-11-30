dc6785b9245608f7856de9c708f2d84e7d9f3ac7
dn12345679 <dylandnnguyen@gmail.com>
2025-11-30 08:01:22 -0800

    switching OS

--- Scripts/main.js
+++ Scripts/main.js
@@ -30,12 +30,18 @@ const s1text = scrolly.querySelector('#scene-1-text');
 const co2 = scrolly.querySelector('.animated-co2-particle');
 const o2 = scrolly.querySelector('.animated-o2-particle');
 const intro_text = scrolly.querySelector('#intro-text-pin');
+/* PRELOAD ITEMS SCENE 2 */
+const text_box_s2 = scrolly.querySelectorAll('.main-text-scene-2.animate-text')
 /* PRELOAD ITEMS SCENE 3*/
 const env3 = scrolly.querySelector('#part-3-env');
 const logs = scrolly.querySelectorAll('.log');
+const axes3 = scrolly.querySelector('.bar-plot');
+/* PRELOAD ITEMS SCENE 4 */
+const text_box_s4 = scrolly.querySelectorAll('.main-text-scene-4.animate-text')
 /* Preload Items Scene 5*/
 const leaf = scrolly.querySelector('.leaf-s5');
 const scatter = scrolly.querySelector('.scatter-s5');
+const bush = scrolly.querySelector('.bush-parallax');
 
 
 
@@ -43,22 +49,90 @@ function handleStepProgress(response) {
     switch (response.element.id) {
         case 'part-1':
             scene1(response)
+            break;
+        case 'part-2':
+            scene2(response)
+            break;
         case 'part-3':
             scene3(response)
+            break;
+        case 'part-4':
+            scene4(response) 
+            break;
         case 'part-5':
             scene5(response)
+            break;
+        case 'part-7':
+            scene7(response)
+            break;
         default: 
             break;
     }
 }
-function scene5(response) {
+
+// text content
+function scene2(response) {
+    const elements = [...text_box_s2];
+    if (response.progress > 0.1) {
+        elements.forEach(el => {
+            el.classList.add("play-fade-in")
+        });
+
+    }
+    else {
+        elements.forEach(el => {
+            el.classList.remove('play-fade-in');
+        });
+    }
+}
+
+function scene4(response) {
+    const elements = [...text_box_s4]
+    if (response.progress > 0.1) {
+        elements.forEach(el => {
+            el.classList.add("play-fade-in")
+        });
+    }
+    else {
+        elements.forEach(el => {
+            el.classList.remove('play-fade-in');
+        })
+    }
+}
+
+function scene7(response) {
+    var map = L.map('map').setView([43.53, 5.45], 12);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    
+    var circle = L.circle([43.53, 5.45], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(map);
+
+}
+
+
+// visualizations
+function scene5(response) { 
 
     if (response.progress < 0.4) {
         leaf.style.left = `${50 + 20 * Math.sin(4 * Math.PI * response.progress)}vw`;
-        leaf.style.scale = 1.0
+        leaf.style.scale = 1.0;
+        bush.style.top = `${140 - 100 * (1- response.progress)}vh`;
     }
-    else if (response.progress > 0.4 && response.progress < 0.6) {
-        leaf.style.scale = `${(0.4)/(response.progress)}`
+    else if (response.progress >= 0.4 && response.progress < 0.6) {
+        const t = (response.progress - 0.4) / (0.6 - 0.4)
+        leaf.style.scale = `${1 - t}`
+        scatter.style.scale = `${t}`
+        scatter.style.opacity = `${t}`
+        bush.style.top = `${140 - 100 * (1- response.progress)}vh`;
+
+        // IDEA: Maybe include layer-blurred "trees" that acts as parallax effects
     }
     else if (response.progress > 0.6) {
         
@@ -69,22 +143,27 @@ function scene5(response) {
 
 function scene3(response) {
     const elements = [env3, ...logs];
+    const t = (response.progress - 0.3) / (0.7 - 0.3)
+    if (Math.sign(t) >0) {
+        axes3.style.opacity = t
+    }
     if (response.progress > 0.7) {
          if (!logs[2].classList.contains('.start-bar')) {
             logs[2].classList.add('start-bar')
 
         }       
     }
-    if (response.progress > 0.6) {
+    else if (response.progress > 0.6) {
 
         if (!logs[1].classList.contains('.start-bar')) {
             logs[1].classList.add('start-bar')
         }
     }
-    if (response.progress > 0.5) {
+    else if (response.progress > 0.5) {
 
         if (!logs[0].classList.contains('.start-bar')) {
             logs[0].classList.add('start-bar')
+            
         }
         
     }
